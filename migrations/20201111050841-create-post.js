b00482272be8a5572f8dbc86b78fd4ab88753a55
seungcheol <astartes22@gmail.com>
2020-11-11 16:17:46 +0900

    feature#7

--- migrations/20201111050841-create-post.js
+++ migrations/20201111050841-create-post.js
@@ -8,9 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userid: {
-        type: Sequelize.INTEGER
-      },
       location: {
         type: Sequelize.STRING
       },
