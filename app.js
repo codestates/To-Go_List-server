2e42e4a1f5112696e2a9d70e7ff1576d67f4c5cf
seungcheol <astartes22@gmail.com>
2020-11-11 17:07:18 +0900

    feature#5 add routes

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+
+const userRouter = require('./routes/users');
+const postRouter = require('./routes/posts');
+const hashtagRouter = require('./routes/hashtags');
+
+const morgan = require('morgan');
+
 const app = express();
 const port = 3001;
+
+app.use(
+    session({
+        secret: '$to-go-list-is-aWeSOmE!',
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+app.use(express.json());
+app.use(
+    cors({
+        methods: ['GET', 'POST', 'PATCH'],
+        credentials: true
+    })
+);
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello I\'m In Docker Container Now!');
 });
 
+app.use('/user', userRouter);
+app.use('/post', postRouter);
+app.use('/hashtag', hashtagRouter);
+
 app.listen(port, () => {
     console.log(`Express server is listening on PORT ${port}`);
 });
+
+module.exports = app;
