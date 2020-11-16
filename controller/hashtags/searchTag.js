4394df1643243eaf2df26e1965f6fd0d92d8cccf
seungcheol <astartes22@gmail.com>
2020-11-16 17:34:55 +0900

    feature#4:add wildcard search

--- controller/hashtags/searchTag.js
+++ controller/hashtags/searchTag.js
@@ -1,25 +1,44 @@
-const { hashtag, post_hashtag, post } = require('../../models');
+const { hashtag, post } = require('../../models');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 
 module.exports = {
     post: async (req, res) => {
         const { userid } = req.session;
         const { query } = req.body;
 
-        if (userid) {
-            await hashtag.findAll({
-                where: { tag: query },
-                include: {
-                    model: post,
-                    through: { attributes: [] }
+        // if (userid) {
+        await hashtag.findAll({
+            where: {
+                tag: {
+                    [Op.like]: '%' + query + '%'
                 }
-            }).then(result => {
-                const posts = [];
+            },
+            include: {
+                model: post,
+                through: { attributes: [] }
+            }
+        })
+            .then(result => result.map(record => record.get({ plain: true })))
+            .then(result => {
+                const temp = [];
+
                 for (let el of result) {
-                    posts.push(el.posts[0]);
+                    temp.push(el.posts[0]);
                 }
+
+                const posts = temp.reduce((acc, cur) => {
+                    if (!acc.some(obj => obj.id === cur.id)) {
+                        acc.push(cur);
+                    }
+                    return acc;
+                }, []);
+
                 res.status(200).json(posts);
+
             }).catch(err => res.status(500).send(err));
-        }
+
+        // }
 
     }
 };
