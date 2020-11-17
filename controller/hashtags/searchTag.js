9d33309ce7070ab48c54bfe691e74343ce417488
seungcheol <astartes22@gmail.com>
2020-11-17 14:32:06 +0900

    delete remark

--- controller/hashtags/searchTag.js
+++ controller/hashtags/searchTag.js
@@ -7,38 +7,36 @@ module.exports = {
         const { userid } = req.session;
         const { query } = req.body;
 
-        // if (userid) {
-        await hashtag.findAll({
-            where: {
-                tag: {
-                    [Op.like]: '%' + query + '%'
-                }
-            },
-            include: {
-                model: post,
-                through: { attributes: [] }
-            }
-        })
-            .then(result => result.map(record => record.get({ plain: true })))
-            .then(result => {
-                const temp = [];
-
-                for (let el of result) {
-                    temp.push(el.posts[0]);
-                }
-
-                const posts = temp.reduce((acc, cur) => {
-                    if (!acc.some(obj => obj.id === cur.id)) {
-                        acc.push(cur);
+        if (userid) {
+            await hashtag.findAll({
+                where: {
+                    tag: {
+                        [Op.like]: '%' + query + '%'
                     }
-                    return acc;
-                }, []);
+                },
+                include: {
+                    model: post,
+                    through: { attributes: [] }
+                }
+            })
+                .then(result => result.map(record => record.get({ plain: true })))
+                .then(result => {
+                    const temp = [];
 
-                res.status(200).json(posts);
+                    for (let el of result) {
+                        temp.push(el.posts[0]);
+                    }
 
-            }).catch(err => res.status(500).send(err));
+                    const posts = temp.reduce((acc, cur) => {
+                        if (!acc.some(obj => obj.id === cur.id)) {
+                            acc.push(cur);
+                        }
+                        return acc;
+                    }, []);
 
-        // }
+                    res.status(200).json(posts);
 
+                }).catch(err => res.status(500).send(err));
+        }
     }
 };
